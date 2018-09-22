45b26505dc1ee9796b86ebe27671e4dfe1d229af
wanghui <fenglicheng1@163.com>
2018-09-22 16:42:52 +0800

    第二部分代码，新增功能：分页、增、删

--- src/main/java/pers/wh/springboot/react/repository/EmployeeRepository.java
+++ src/main/java/pers/wh/springboot/react/repository/EmployeeRepository.java
@@ -1,7 +1,8 @@
 package pers.wh.springboot.react.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import pers.wh.springboot.react.entity.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,10 @@ module.exports = {
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react',"stage-0"]
                 }
             }
         ]
